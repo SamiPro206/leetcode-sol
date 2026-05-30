aa08147fe4b8e70d90cda0d8afe5d3940315a414
Sami Kabbaj <kabbajsami2006@gmail.com>
2026-05-30 07:08:46 +0200

    feat: add python template

---
+++ python/template.py
@@ -0,0 +1,38 @@
+"""
+Problem: <number> - <title>
+Difficulty: <Easy | Medium | Hard>
+
+Link:
+https://leetcode.com/problems/<problem-name>/
+
+Pattern:
+- TODO
+
+Time Complexity:
+O(...)
+
+Space Complexity:
+O(...)
+
+Notes:
+- TODO
+"""
+
+
+class Solution:
+    pass
+
+
+def run_tests():
+    solution = Solution()
+
+    # Test 1
+    # assert solution.method(...) == ...
+
+    # ...
+
+    print("All tests passed.")
+
+
+if __name__ == "__main__":
+    run_tests()
