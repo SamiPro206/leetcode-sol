a535bb89251dc8d5232e3913037561d8390c9e5c
Sami Kabbaj <kabbajsami2006@gmail.com>
2026-06-03 16:46:15 +0200

    feat: add problem 1

---
+++ main/0001_two_sum.py
@@ -0,0 +1,55 @@
+"""
+Problem: 1 - Two Sum
+Difficulty: Easy
+
+Link:
+https://leetcode.com/problems/two-sum/
+
+Time Complexity:
+O(n)
+
+Space Complexity:
+O(n)
+
+Notes:
+"""
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        targets = {}
+
+        for i, num in enumerate(nums):
+            complement = target-num
+            if complement in targets:
+                return [targets[complement], i]
+            targets[num] = i
+
+def run_tests():
+    s = Solution()
+
+    # Test 1
+    nums = [2,7,11,15]
+    target = 9
+    assert s.twoSum(nums, target) == [0, 1]
+
+    # Test 2
+    nums = [3,2,4]
+    target = 6
+    assert s.twoSum(nums, target) == [1, 2]
+
+    # Test 3
+    nums = [3,3]
+    target = 6
+    assert s.twoSum(nums, target) == [0, 1]
+
+    print("All tests passed.")
+
+
+if __name__ == "__main__":
+    run_tests()
